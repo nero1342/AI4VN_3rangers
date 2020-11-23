2ef93abdf2672d1faaa0021ad7631c883cc6d377
E-Ro Nguyen <nero18@apcs.vn>
2020-11-23 11:13:06 +0700

    baseline

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,4 +1,3 @@
 from .classification.crossentropy import BCEWithLogitsLoss, WeightedBCEWithLogitsLoss, CrossEntropyLoss
 from .classification.focalloss import FocalLoss
-from .segmentation.diceloss import DiceLoss
 from .mixedloss import MixedLoss
